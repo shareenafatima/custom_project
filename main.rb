5f05dd6eca17a252efb6b052526a4fcafee1a704
shareena fatima <shareenafatima40@gmail.com>
2024-01-03 04:55:54 +0500

    my_each_with_index

--- main.rb
+++ main.rb
@@ -6,7 +6,17 @@ module Enumerable
             self
         end
     end
+
+    def my_each_with_index
+        self.is_a?(Array)
+        self.size.times {|index| yield self[index],index}
+        self
+    end
 end
 
 num = [1,2,3,4,5,6,7,8,9,10]
+puts "my_each\n"
 num.my_each {|i| p i}
+puts "\n"
+puts "my_each_with_index"
+num.my_each_with_index {|i,j| p "#{j} : #{i}"}
